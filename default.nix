5034c553fe3916aba92d0e5819ca2348884d6a90
Tim Cuthbertson <tim@gfxmonk.net>
2019-10-29 21:39:06 +1100

    make nix buildable

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
-	name="statusprompt";
+	name="status-check";
 	buildInputs = with ocamlPackages;
 		[ ocaml ocamlbuild findlib ];
+	installPhase = "bash ./install.sh $out";
+	src = builtins.fetchGit { url = ./.; };
 }
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
 mkdir -p "$1/bin"
 cp --dereference main.native "$1"/bin/status-check
